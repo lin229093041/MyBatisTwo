407b3c9b87df58bac21bf955a8bdf302f7a30dc9
rony.lin <linruoyu1212@qq.com>
2019-08-05 16:00:50 +0800

    mybatis type类型为复杂类型

---
+++ src/com/rony/test/UserQueryInfo.java
@@ -0,0 +1,22 @@
+package com.rony.test;
+
+import com.rony.po.User;
+
+public class UserQueryInfo {
+    private User u;
+
+    public User getU() {
+        return u;
+    }
+
+    public void setU(User u) {
+        this.u = u;
+    }
+
+    public UserQueryInfo(User u) {
+        this.u = u;
+    }
+
+    public UserQueryInfo() {
+    }
+}
