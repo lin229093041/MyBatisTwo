4bd5a29ef61b39a69e9570de58f8f16276cc48fc
rony.lin <linruoyu1212@qq.com>
2019-08-05 16:01:45 +0800

    mybatis mapper配置文件详解

--- src/com/rony/test/Test.java
+++ src/com/rony/test/Test.java
@@ -16,14 +16,20 @@ public class Test {
         SqlSession session= new SqlSessionFactoryBuilder().build(is).openSession();
 //        User u=session.selectOne("test.selectUser",1);
         User u=new User(3,"小李",19,"123456");
-        int i = session.insert("test.insertUser", u);
+//        int i = session.insert("test.insertUser", u);
 //        System.out.println("小红id："+u.getId());
 
 //        session.delete("test.deleteUser",4);
 //        session.update("test.updateUser",u);
-        session.commit();
+//        session.commit();
 //        List<User> list=session.selectList("test.selectByName","r");
 //        System.out.println(list);
+//        UserQueryInfo info = new UserQueryInfo(u);
+//        List list=session.selectList("test.QueryUser",2);
+//        System.out.println(list);
+//        session.selectM
+        List<Object> list = session.selectList("test.selectRename");
+        System.out.println(list);
     }
 
     public void test(){
---
+++ test/com/rony/test/UserQueryInfoTest.java
@@ -0,0 +1,14 @@
+package com.rony.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserQueryInfoTest {
+
+    @org.junit.jupiter.api.Test
+    void getU() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setU() {
+    }
+}
